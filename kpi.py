3b1f3ba55219d1e0568eb50c75107ea4be5d06d3
andersondbk <32954765+andersondbk@users.noreply.github.com>
2021-10-05 13:04:44 +0700

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -84,7 +84,9 @@ st.subheader("")
 st.markdown(f"<h1 style='text-align: center; color: black;'>{Method}</h1>", unsafe_allow_html=True)
 st.markdown(f"<h2 style='text-align: left; color: black;'>{CALCULATION}</h2>", unsafe_allow_html=True)
 
-st.markdown("$$ CAC formula = Total Costs to Aqcuire Customers / Number of Customers$$")
+st.markdown("$$
+            CAC formula = Total Costs to Aqcuire Customers / Number of Customers
+            $$")
 
 st.markdown(" ## Gross Profit after CAC")
 st.image("GPAfterCAC.png")
