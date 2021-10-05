0b34ba2e46ed3150ee41aa513284585df9ffbaa0
andersondbk <32954765+andersondbk@users.noreply.github.com>
2021-10-05 12:09:25 +0700

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -14,27 +14,28 @@ st.set_page_config(
 
 st.markdown(" # Overview of Inversion Customer Aquisition Costs Relationship")
 st.image("fb-customer-acquisition-cost.png")
-subheader = "If Marketing Activities and Lead Generation tactics are not well thought out, they can drive up the Cost of Customer Acquisition (CAC) and erode you profit margin. Are you spending money and time unnecessarily on the wrong people? It's important to distinguish between:"  
-st.markdown(f"<h1 style='text-align: center; color: black;'>{subheader}</h1>", unsafe_allow_html=True)
+subheader = "If Marketing Activities and Lead Generation tactics are not well thought out, they can drive up the Cost of Customer Acquisition (CAC) and erode your profit margin. Are you spending money and time unnecessarily on the wrong people? It's important to distinguish between:"  
+st.markdown(f"<h2 style='text-align: center; color: black;'>{subheader}</h1>", unsafe_allow_html=True)
 
 col1, col2,col3= st.columns(3)
 subsub = "Customers attracted by MARKETING CONTENT (Newsletters, Promotions, Seminars etc)"
+mqlsql = "Marketing Qualified Leads  VS  Sales Qualified Leads"
 subsub2 = "Customers who show an ACTIVE INTEREST in purchasing a product (Request a quote, Fill out an CRM form)"
 
 with col1:
-    st.markdown(f"<h2 style='text-align: left; color: black;'>{subsub}</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='text-align: left; color: black;'>{subsub}</h2>", unsafe_allow_html=True)
     
 with col2:
-    pass
+    st.markdown(f"<h4 style='text-align: left; color: black;'>{mqlsql}</h2>", unsafe_allow_html=True)
 with col3: 
-    st.markdown(f"<h2 style='text-align: right; color: black;'>{subsub2}</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='text-align: right; color: black;'>{subsub2}</h2>", unsafe_allow_html=True)
     
 
 st.image("MQL-vs-SQL.jpeg")
      
 
-conclusion = "Are you spending time and money on the wrong section of your customer base?" 
-conclusion2 = "By focussing on SQL lead generation, you can decreases you CAC which will increase your profit per customer"  
+conclusion = "Are you spending time and money on people interested in your marketing content and not enough on people who are actively interested in purchasing your product or service? Sometime we are interested in a businesses product, and will sign up for the newsletter but we may never intend (or could never afford) to buy the product." 
+conclusion2 = "By focussing on SQL lead generation, and allocating your resources to SQLs, you can decreases you CAC which will increase your profit per customer"  
 st.markdown(f"<h3 style='text-align: left; color: black;'>{conclusion}</h3>", unsafe_allow_html=True)
 st.markdown(f"<h3 style='text-align: left; color: black;'>{conclusion2}</h3>", unsafe_allow_html=True)
 st.write("")
@@ -61,9 +62,10 @@ st.write("")
 
 st.write("---")
 
+st.subheader("Consider all of the costs related to Customer Aquisition. An example can be seen above")
+
 st.image("CAC COSTS.png")
     
-st.subheader("Consider all of the costs related to Customer Aquisition. An example can be seen above")
 
 st.write("---")
 
