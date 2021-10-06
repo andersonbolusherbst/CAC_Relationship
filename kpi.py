99ea36a3756017741f18d6337d1f484bb7b493ce
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2021-10-06 07:52:13 +0700

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -37,8 +37,8 @@ with col3:
 
 conclusion = "It's easy to be unaware that you are spending time and money on people interested in your marketing content and not enough on people who are actively interested in purchasing your product or service. Sometimes we are interested in a businesses product, and will sign up for the newsletter but we may never intend (or could never afford) to buy the product." 
 conclusion2 = "By focusing on SQL lead generation, and allocating your resources to SQLs, you can decreases your CAC which will increase your profit per customer"  
-st.markdown(f"<h3 style='text-align: left; color: black;'>{conclusion}</h3>", unsafe_allow_html=True)
-st.markdown(f"<h3 style='text-align: left; color: black;'>{conclusion2}</h3>", unsafe_allow_html=True)
+st.markdown(f"<h4 style='text-align: left; color: black;'>{conclusion}</h4>", unsafe_allow_html=True)
+st.markdown(f"<h4 style='text-align: left; color: black;'>{conclusion2}</h4>", unsafe_allow_html=True)
 st.write("")
 st.write("")
 
