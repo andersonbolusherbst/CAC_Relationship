1a99d18c6eeadb92cb5b659b44490fe37647cffa
andersondbk <32954765+andersondbk@users.noreply.github.com>
2021-10-06 08:52:28 +0700

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -71,7 +71,8 @@ st.subheader("Now consider the effect of these costs being allocated to people o
     
 
 st.write("---")
-
+st.subheader("The relationship of Customer Acquisition Cost with Profit can best be illustrated with the CAC margin. You can explore the effects of changes in CAC after the next section")
+col1,col2 = st.columns(2)
 WHY = "Why is CAC important?"
 CALCULATION = "CAC Margin Calculation"
 Method = "Method for CAC Calculation"
@@ -82,12 +83,14 @@ Method = "Method for CAC Calculation"
 #st.subheader(" ✔︎ Powerful metric to calculate the resulting ROI from an Investor standpoint")
 #st.subheader("")
 #st.markdown(f"<h1 style='text-align: center; color: black;'>{Method}</h1>", unsafe_allow_html=True)
-st.markdown(f"<h2 style='text-align: left; color: black;'>{CALCULATION}</h2>", unsafe_allow_html=True)
-
-st.markdown("_CAC formula = Total Costs to Aqcuire Customers / Number of Customers_")
-st.markdown("_CAC Margin formula = Total Costs to Aqcuire Customers / Revenue_")
-st.markdown("_Gross Profit After CAC Margin = 1 - CAC Margin_")
-st.subheader("The relationship of Customer Acquisition Cost with Profit can best be illustrated with the CAC margin. You can explore the effects of changes in CAC after the next section")
+with col1:
+    st.subheader("CAC Margin Calculation")
+    st.markdown("_CAC formula = Total Costs to Aqcuire Customers / Number of Customers_")
+    st.markdown("_CAC Margin formula = Total Costs to Aqcuire Customers / Revenue_")
+    st.markdown("_Gross Profit After CAC Margin = 1 - CAC Margin_")
+    
+with col2:
+    st.write("upload cac margin image here")
 
 
                             
